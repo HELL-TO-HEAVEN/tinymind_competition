b7050d970db7a2afb6f785c84b09bb7786a7cfa7
cgh <44086116+HELL-TO-HEAVEN@users.noreply.github.com>
2020-03-15 20:43:10 +0800

    Update downmodel.sh

--- downmodel.sh
+++ downmodel.sh
@@ -1,7 +1,7 @@
 
 echo 'start download the pretrained model: resnext-50'
 
-wget -P ./models "http://data.dmlc.ml/models/imagenet/resnext/50-layers/resnext-50-0000.params"
-wget -P ./models "http://data.dmlc.ml/models/imagenet/resnext/50-layers/resnext-50-symbol.json"
+wget -P ./models "http://data.mxnet.io/models/imagenet/resnext/50-layers/resnext-50-0000.params"
+wget -P ./models "http://data.mxnet.io/models/imagenet/resnext/50-layers/resnext-50-symbol.json"
 
 echo 'over !'
